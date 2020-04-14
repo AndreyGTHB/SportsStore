2e6977101fc391ef313e7183ef5263fca12e93bc
AndreyGTHB <andr.g.tuk@gmail.com>
2020-04-14 17:46:48 +0700

    Product list added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {StoreComponent} from './store/store.component';
+import {StoreModule} from './store/store.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    StoreComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    StoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
