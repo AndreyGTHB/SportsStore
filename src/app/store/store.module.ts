bdadf430c3e36f6b9024dff4bdf97ed67bfb8597
Grigory <grigory@clearscale.net>
2020-04-18 20:13:06 +0700

    Cart added

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -4,10 +4,12 @@ import {FormsModule} from '@angular/forms';
 import {ModelModule} from '../model/model.module';
 import {StoreComponent} from './store.component';
 import {CounterDirective} from './counter.directive';
+import { CartSummaryComponent } from './cart-summary/cart-summary.component';
+import { CartDetailComponent } from './cart-detail/cart-detail.component';
 
 @NgModule({
   imports: [ModelModule, BrowserModule, FormsModule],
-  declarations: [CounterDirective, StoreComponent],
+  declarations: [CounterDirective, StoreComponent, CartSummaryComponent, CartDetailComponent],
   exports: [StoreComponent]
 })
 export class StoreModule { }
